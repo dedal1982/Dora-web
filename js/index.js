99273e4d75fa17e2f65f3e38dc954564fdb5ccc6
Al Dm <d44.a1@yandex.ru>
2025-01-21 23:59:20 +0300

    1

--- js/index.js
+++ js/index.js
@@ -6,16 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
     item.addEventListener("click", function () {
       const tabId = this.getAttribute("data-tab");
 
-      // Удаляем класс "active" у всех li
       taskInfoSelectItems.forEach((li) => li.classList.remove("active"));
 
-      // Добавляем класс "active" к текущему li
       this.classList.add("active");
 
-      // Удаляем класс "active" у всех skills__page
       skillsPages.forEach((page) => page.classList.remove("active"));
 
-      // Добавляем класс "active" к соответствующему skills__page
       document
         .querySelector(`.skills__page[data-page="${tabId}"]`)
         .classList.add("active");
